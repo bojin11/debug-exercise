4f18d5f5462ae534fc2d069e6342840fb60119f6
bojin11 <15011278723@163.com>
2022-01-27 10:05:14 +0000

    changed the test

--- client/Test/layout.spec.js
+++ client/Test/layout.spec.js
@@ -1,3 +1,6 @@
+/**
+* @jest-environment jsdom
+*/
 const fs = require('fs');
 const path = require('path');
 const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
@@ -11,7 +14,7 @@ describe('index.html', () => {
 
     describe('text', () => {
         test('it has correct text', () => {
-            const title = document.querySelector('text');
+            const title = document.querySelector('#text');
             expect(title).toBeTruthy();
             expect(title.textContent).toBe("Motivational Quotes")
         })
